6a18948c4d4c7fbcbb929c06187b75f8c7decf12
007MJ <k.mijiza@gmail.com>
2024-09-26 21:00:06 -0400

    avec le printf

--- src/game/raycater.c
+++ src/game/raycater.c
@@ -3,14 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   raycater.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pnsaka <pnsaka@student.42.fr>              +#+  +:+       +#+        */
+/*   By: mnshimiy <mnshimiy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 19:39:40 by peternsaka        #+#    #+#             */
-/*   Updated: 2024/09/26 13:12:46 by pnsaka           ###   ########.fr       */
+/*   Updated: 2024/09/26 20:59:09 by mnshimiy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cube.h"
+void stepPrintf(t_cube *game)
+{	
+	printf("=====================step function====================\n");
+	printf("ray_dir_x	: %f\n", game->ray_dir_x);
+	printf("ray_dir_y	: %f\n", game->ray_dir_y);
+	printf("side_dist_x : %f\n", game->side_dist_x);
+	printf("side_dist_y : %f\n", game->side_dist_y);
+	printf("step_x		: %d\n", game->step_x);
+	printf("step_y		: %d\n", game->step_y);
+	printf("delta_x		: %f\n", game->delta_x);
+	printf("delta_y		: %f\n", game->delta_y);
+	printf("map_x		: %d\n", game->map_x);
+	printf("map_y		: %d\n", game->map_y);
+}
 
 void 	step(t_cube *game)
 {
@@ -34,7 +48,23 @@ void 	step(t_cube *game)
 		game->step_y = 1;
 		game->side_dist_y = (game->map_y + 1.0 - game->player_y) * game->delta_y;
 	}
+	stepPrintf(game);
 }
+void ddaPrintf(t_cube *game)
+{	
+	printf("=====================dda function====================\n");
+	printf("side_dist_x		: %f\n", game->side_dist_x);
+	printf("side_dist_y		: %f\n", game->side_dist_y);
+	printf("map_x			: %d\n", game->map_x);
+	printf("map_y			: %d\n", game->map_y);
+	printf("side			: %d\n", game->side);
+	printf("step_x			: %d\n", game->step_x);
+	printf("step_y			: %d\n", game->step_y);
+	printf("delta_x			: %f\n", game->delta_x);
+	printf("delta_y			: %f\n", game->delta_y);
+	printf("=====================dda end====================\n");
+}
+
 
 void	dda(t_cube *game)
 {
@@ -64,6 +94,16 @@ void	dda(t_cube *game)
 		game->perp_dist = (game->side_dist_x - game->delta_x);
 	else
 		game->perp_dist = (game->side_dist_y - game->delta_y);
+		ddaPrintf(game);
+}
+void dist_calcPrintf(t_cube *game)
+{	
+	printf("=====================dist_calc function====================\n");
+	printf("delta_x		: %f\n", game->delta_x);
+	printf("delta_y		: %f\n", game->delta_y);
+	printf("ray_dx		: %f\n", game->ray_dx);
+	printf("ray_dy		: %f\n", game->ray_dy);
+	printf("=====================dist_calc end====================\n");
 }
 
 void	dist_calc(t_cube *game)
@@ -76,6 +116,22 @@ void	dist_calc(t_cube *game)
 		game->delta_y = 1e30;
 	else
 		game->delta_y = fabs(1 / game->ray_dir_y);
+		dist_calcPrintf(game);
+}
+
+void wall_heightPrintf(t_cube *game){
+	printf("===================== wall_height function====================\n");
+	printf("game side	: %d\n", game->side);
+	printf("wall_dist	: %f\n", game->wall_dist);
+	printf("map_x		: %d\n", game->map_x);
+	printf("map_y		: %d\n", game->map_y);
+	printf("player_x	: %f\n" , game->player_x);
+	printf("player_y	: %f\n", game->player_y);
+	printf("draw_start	: %d\n", game->draw_start);
+	printf("draw_end	: %d\n", game->draw_end);
+	printf("ray_dy		: %f\n", game->ray_dy);
+	printf("wall_x		: %f\n", game->wall_x);
+	printf("===================== wall_height end====================\n");
 }
 
 void	wall_height(t_cube *game)
@@ -100,6 +156,19 @@ void	wall_height(t_cube *game)
 	else
 		game->wall_x = game->player_x + game->wall_dist * game->ray_dx;
 	game->wall_x -= floor(game->wall_x);
+	wall_heightPrintf(game);
+}
+void castingPrintf(t_cube *game){
+	printf("===================== wall_height function====================\n");
+	printf("fov_ratio	: %f\n", game->fov_ratio);
+	printf("dir_x	: %f\n", game->dir_x);
+	printf("dir_y		: %f\n", game->dir_y);
+	printf("map_y		: %d\n", game->map_y);
+	printf("player_x	: %f\n" , game->player_x);
+	printf("player_y	: %f\n", game->player_y);
+	printf("cam_x	: %f\n", game->cam_x);
+	printf("cam_y	: %f\n", game->cam_y);
+	printf("===================== wall_height end====================\n");
 }
 
 void	casting(t_cube *game)
